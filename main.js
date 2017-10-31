c12a4dfcbcd6e037ad97e0ffee7ec31c4f2288dc
jersh <Josh@zarvox.local>
2017-10-31 12:27:00 -0400

    Added colours, rink selection, and keyboard controls.

--- main.js
+++ main.js
@@ -5,6 +5,15 @@ var pause = false;
 var reset = true;
 var lastChecked = new Date().getTime();
 var breaks = 3;
+var rinks = 1;
+
+var colorList = [
+  "rgb(168, 40, 40)",
+  "rgb(83, 208, 32)",
+  "rgb(38, 142, 217)",
+  "rgb(244, 139, 43)"
+]
+
 
 var bell = new Audio('bell.mp3');
 var buzz = new Audio('buzz.mp3');
@@ -15,9 +24,117 @@ window.onload = function(){
     $(this).html(parseInt($(this).html())+1);
   });
 
-  $('.minus').click(function(e){
-    $(this).parent().children('.count').html(parseInt($(this).parent().children('.count').html())-1);
+  $('.name').click(function(e){
+    var newColor = colorList[colorList.indexOf($(this).css('background-color')) + 1];
+    if(newColor == undefined){
+      newColor = colorList[0];
+    }
+    $(this).css('background-color',newColor);
+    $(this).parent().find('.minus').css('background-color',newColor);
   });
+
+  $('.minus').click(function(e){
+    var value = parseInt($(this).parent().children('.count').html())-1;
+    if( value < 0 ){
+      value = 0;
+    }
+    $(this).parent().children('.count').html(value);
+  });
+
+  $(document).keypress(function(e){
+    if(e.which == 32){
+      if(pause == false){
+        pauseTimer();
+      } else {
+        startTimer();
+      }
+    } else if(e.which == 114) {
+      resetTimer();
+    } else if(e.which == 49){ //1
+      if(rinks == 1){
+        $('.count:eq(0)').html(parseInt($('.count:eq(0)').html())+1);
+      } else {
+        $('.count:eq(2)').html(parseInt($('.count:eq(2)').html())+1);
+      }
+    } else if(e.which == 33){ //!
+      if(rinks == 1){
+        var value = parseInt($('.count:eq(0)').parent().children('.count').html())-1;
+        if( value < 0 ){
+          value = 0;
+        }
+        $('.count:eq(0)').parent().children('.count').html(value);
+      } else {
+        var value = parseInt($('.count:eq(2)').parent().children('.count').html())-1;
+        if( value < 0 ){
+          value = 0;
+        }
+        $('.count:eq(2)').parent().children('.count').html(value);
+      }
+    } else if(e.which == 50){//2
+      if(rinks == 1){
+        $('.count:eq(1)').html(parseInt($('.count:eq(1)').html())+1);
+      } else {
+        $('.count:eq(3)').html(parseInt($('.count:eq(3)').html())+1);
+      }
+    } else if(e.which == 64){ //@
+      if(rinks == 1){
+        var value = parseInt($('.count:eq(1)').parent().children('.count').html())-1;
+        if( value < 0 ){
+          value = 0;
+        }
+        $('.count:eq(1)').parent().children('.count').html(value);
+      } else {
+        var value = parseInt($('.count:eq(3)').parent().children('.count').html())-1;
+        if( value < 0 ){
+          value = 0;
+        }
+        $('.count:eq(3)').parent().children('.count').html(value);
+      }
+    } else if(e.which == 51){//3
+      if(rinks == 1){
+        //nothing
+      } else {
+        $('.count:eq(4)').html(parseInt($('.count:eq(4)').html())+1);
+      }
+    } else if(e.which == 35){//#
+      if(rinks == 1){
+        //nothing
+      } else {
+        var value = parseInt($('.count:eq(4)').parent().children('.count').html())-1;
+        if( value < 0 ){
+          value = 0;
+        }
+        $('.count:eq(4)').parent().children('.count').html(value);
+      }
+    } else if(e.which == 52){//4
+      if(rinks == 1){
+        //nothing
+      } else {
+        $('.count:eq(5)').html(parseInt($('.count:eq(5)').html())+1);
+      }
+    } else if(e.which == 36){//$
+      if(rinks == 1){
+        //nothing
+      } else {
+        var value = parseInt($('.count:eq(5)').parent().children('.count').html())-1;
+        if( value < 0 ){
+          value = 0;
+        }
+        $('.count:eq(5)').parent().children('.count').html(value);
+      }
+    }
+  });
+}
+
+function help(){
+  alert('Click the scores to increase score. Click the minus to descrease score.');
+  alert('You can use they 1-4 keys on your keyboard to increase, and Shift + 1-4 to decrease as well');
+  alert('Click the top part of a scorecard to change the color of the team.');
+  alert('Click the play or pause button to start or stop the timer. Space works as well.');
+  alert('Press the gear to change the time and number of breaks in the game.');
+  alert('Press r or click the refresh icon to reset timer to default and scores to zero.');
+  alert('Click the fullscreen button to make the gameboard full screen.');
+  alert('Click the hastag to change number of rinks.');
 }
 
 function startTimer(){
@@ -66,6 +183,18 @@ function resetTimer(){
 function pauseTimer(){
   pause = true;
 
+}
+
+function moreRinks(){
+  if(rinks == 2){
+    $('.twoRinks').hide();
+    $('.oneRinks').show();
+    rinks = 1;
+  } else if(rinks == 1){
+    $('.twoRinks').show();
+    $('.oneRinks').hide();
+    rinks = 2;
+  }
 }
 
 function changeTimer(){
